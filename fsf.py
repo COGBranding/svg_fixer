9bb86a9af16ca634ef5187a4e59abe9ec6e7413d
prabhat giri <prabhat.giri@cogdigital.com.au>
2022-09-07 15:28:24 +1000

    script to fix svg missingxml encoding tag

---
+++ fsf.py
@@ -0,0 +1,18 @@
+import os
+directory_list = '.'
+ext = ('.svg')
+for files in os.listdir(directory_list):
+    if files.endswith(ext):
+        one_line = '<?xml version="1.0" encoding="utf-8"?>\t\n'
+        with open(files, 'r+') as fp:
+            f_line = fp.readline().rstrip()
+            lines = fp.readlines()
+            content = fp.read()
+            if( f_line != one_line.rstrip()):
+                lines.insert(0, one_line)
+                fp.seek(0)              
+                fp.write(one_line + f_line + '\t\n' + content)
+            else:
+                continue
+    else:
+        continue
