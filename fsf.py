6462ee2493cf464568fb07f728365d508b5d9c98
COG Branding <81595974+COGBranding@users.noreply.github.com>
2022-10-04 17:26:18 +1100

    Update fsf.py

--- fsf.py
+++ fsf.py
@@ -3,16 +3,16 @@ directory_list = '.'
 ext = ('.svg')
 for files in os.listdir(directory_list):
     if files.endswith(ext):
-        one_line = '<?xml version="1.0" encoding="utf-8"?>\t\n'
+        one_line = '<?xml version="1.0" encoding="utf-8"?>\n'
         with open(files, 'r+') as fp:
-            f_line = fp.readline().rstrip()
-            lines = fp.readlines()
+            # f_line = fp.readline().rstrip()
+            # lines = fp.readlines()
             content = fp.read()
-            if( f_line != one_line.rstrip()):
-                lines.insert(0, one_line)
-                fp.seek(0)              
-                fp.write(one_line + f_line + '\t\n' + content)
-            else:
-                continue
+            # if( f_line != one_line.rstrip()):
+                # lines.insert(0, one_line)
+            fp.seek(0)              
+            fp.write(one_line + content)
+            # else:
+            #     continue
     else:
         continue
